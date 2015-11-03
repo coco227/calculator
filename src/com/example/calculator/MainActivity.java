007c40304290614364107429fe90c9819868d0e6
李立波 <lilibo@ecloud.com>
2015-11-03 14:26:20 +0800

    完善

--- src/com/example/calculator/MainActivity.java
+++ src/com/example/calculator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.calculator;
 
+import android.R.integer;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +14,10 @@ public class MainActivity extends ActionBarActivity {
 	
 	private static final String EditText = null;
 	EditText editField;
+	
+	int Num_1 = 0;
+	int Num_2 = 0;
+	String sign = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +47,180 @@ public class MainActivity extends ActionBarActivity {
 	}
 	
 	public void inputNum(View view) {
-		
-		EditText num = (EditText)findViewById(R.id.Num);
-		
-		Log.d(null, view.findViewById(R.id.Num).toString());
-		
-		switch () {
-		case value:
-			
+		String resultStr = this.editField.getText().toString();
+		switch (view.getId()) {
+		case R.id._0:
+			if(sign != ""){
+				if(resultStr.length() > 0){
+					resultStr = resultStr + "0";
+					Num_2 = Integer.valueOf(resultStr).intValue();
+					Log.d(null, "----" + resultStr);
+					editField.setText(resultStr);
+				}
+			}else {
+				if(resultStr.length() > 0){
+					resultStr = resultStr + "0";
+					Num_1 = Integer.valueOf(resultStr).intValue();
+					Log.d(null, "----11" + resultStr);
+					editField.setText(resultStr);
+				}
+			}
 			break;
-
+		case R.id._1:
+			if(sign != ""){
+				resultStr = resultStr + "1";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "1";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._2:
+			if(sign != ""){
+				resultStr = resultStr + "2";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "2";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._3:
+			if(sign != ""){
+				resultStr = resultStr + "3";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "3";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._4:
+			if(sign != ""){
+				resultStr = resultStr + "4";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "4";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._5:
+			if(sign != ""){
+				resultStr = resultStr + "5";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "5";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._6:
+			if(sign != ""){
+				resultStr = resultStr + "6";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "6";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._7:
+			if(sign != ""){
+				resultStr = resultStr + "7";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "7";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id._8:
+			if(sign != ""){
+				resultStr = resultStr + "8";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "8";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+		break;
+		case R.id._9:
+			if(sign != ""){
+				resultStr = resultStr + "9";
+				Num_2 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}else {
+				resultStr = resultStr + "9";
+				Num_1 = Integer.valueOf(resultStr).intValue();
+				editField.setText(resultStr);
+			}
+			break;
+		case R.id.toZero:
+			resultStr = "";
+			Num_1 = 0;
+			Num_2 = 0;
+			editField.setText(resultStr);
+			
+		break;
 		default:
 			break;
+		}
+	}
+	
+	public void cal(View view) {
+		
+		if (this.editField.getText().toString().length() > 0) {
+			this.Num_1 = Integer.valueOf(this.editField.getText().toString()).intValue();
+			switch (view.getId()) {
+			case R.id.plus:
+				sign = "plus";
+				this.editField.setText("");
+				break;
+			case R.id.minus:
+				sign = "minus";
+				this.editField.setText("");
+				break;
+			case R.id.times:
+				sign = "times";
+				this.editField.setText("");
+				break;
+			case R.id.divide:
+				sign = "divide";
+				this.editField.setText("");
+				break;
+			case R.id.isEqual:
+				switch (this.sign) {
+				case "plus":
+					this.editField.setText(Integer.toString(Num_1 + Num_2));
+					break;
+				case "minus":
+					this.editField.setText(Integer.toString(Num_1 - Num_2));
+					break;
+				case "times":
+					this.editField.setText(Integer.toString(Num_1 * Num_2));
+					break;
+				case "divide":
+					this.editField.setText(Integer.toString(Num_1 / Num_2));
+					break;
+
+				default:
+					break;
+				}
+				
+				break;
+			default:
+				break;
+			}
 		}
 	}
 }
